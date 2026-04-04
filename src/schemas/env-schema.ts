366daeef0e3139424d4b185eb46a9d6456308705
Tommaso Lisella <tommasolisella99@gmail.com>
2026-04-04 12:08:48 +0200

    fix code

--- src/schemas/env-schema.ts
+++ src/schemas/env-schema.ts
@@ -2,8 +2,10 @@ import * as z from 'zod';
 
 export type Env = z.infer<typeof EnvSchema>;
 
-const _optional = <S extends z.core.SomeType>(schema: S): z.ZodPipe<z.ZodTransform<unknown, unknown>, S> =>
-  z.preprocess((v) => (v === '' ? undefined : v), schema);
+const _optional = <S extends z.ZodType>(
+  schema: S,
+  def: z.core.util.NoUndefined<z.core.output<S>>,
+): z.ZodPipe<z.ZodTransform, z.ZodDefault<S>> => z.preprocess((v) => (v === '' ? undefined : v), schema.default(def));
 
 export const EnvSchema = z.object({
   NODE_ENV: z.enum(['development', 'production']),
